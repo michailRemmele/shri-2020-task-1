9a0780919dec7f3420884ce36435b47ce6e4f86b
Mikhail Remmele <mikhail.remmele@gmail.com>
2020-01-04 14:07:02 +0600

    added watch task into gulp config to rebuild the project on changes in css or js files. Updated npm scripts. Added the possibility to pass dest arg while starting or building the project. Updated readme: added new instructions about running the project and information about new yargs dependency

--- gulpfile.js
+++ gulpfile.js
@@ -3,29 +3,39 @@ const concat = require('gulp-concat');
 const cleanCSS = require('gulp-clean-css');
 const uglify = require('gulp-uglify');
 const del = require('del');
+const argv = require('yargs').argv;
 
 const path = {
   css: './src/**/*.css',
   js: './src/**/*.js',
   build: './build/',
+  watch: './build/**',
 };
 
+const dest = argv.dest ? argv.dest : path.build;
+
 function clean() {
-  return del([ path.build ]);
+  return del([dest]);
 }
 
 function styles() {
   return gulp.src(path.css)
     .pipe(cleanCSS())
     .pipe(concat('style.css'))
-    .pipe(gulp.dest(path.build));
+    .pipe(gulp.dest(dest));
 }
 
 function scripts() {
   return gulp.src(path.js)
     .pipe(uglify())
     .pipe(concat('script.js'))
-    .pipe(gulp.dest(path.build));
+    .pipe(gulp.dest(dest));
 }
 
-exports.default = gulp.series(clean, gulp.parallel(styles, scripts));
+function watch() {
+  gulp.watch(path.css, styles);
+  gulp.watch(path.js, scripts);
+}
+
+exports.build = gulp.series(clean, gulp.parallel(styles, scripts));
+exports.default = gulp.series(clean, gulp.parallel(styles, scripts), watch);
