0c192f37ff6acf05e7c2b8e385348e615efa349f
Mikhail Remmele <mikhail.remmele@gmail.com>
2020-01-05 20:24:57 +0600

    added sass support

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,12 @@
 const gulp = require('gulp');
 const concat = require('gulp-concat');
+const sass = require('gulp-sass');
 const cleanCSS = require('gulp-clean-css');
 const uglify = require('gulp-uglify-es').default;
 const argv = require('yargs').argv;
 
 const path = {
-  css: './src/**/*.css',
+  css: './src/**/*.scss',
   js: './src/**/*.js',
   build: './build/',
   watch: './build/**',
@@ -15,6 +16,7 @@ const dest = argv.dest ? argv.dest : path.build;
 
 function styles() {
   return gulp.src(path.css)
+    .pipe(sass().on('error', sass.logError))
     .pipe(cleanCSS())
     .pipe(concat('style.css'))
     .pipe(gulp.dest(dest));
